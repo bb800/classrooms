87d4f8bb200fc7d94427d3a26b92370f5108fe81
qiyu <qiyu.sg@gmail.com>
2021-05-25 15:45:05 +0800

    Added tests, secondary sort utility function

--- __tests__/repo_registrations.test.js
+++ __tests__/repo_registrations.test.js
@@ -1,6 +1,6 @@
 const { Repository } = require('../server/db/repository');
 const { getTestConnectionPool } = require('../server/db/configurations');
-const { sortResultsByKey, filterObject } = require('../server/utils/compare');
+const { sortResultsBy2Keys, filterObject } = require('../server/utils/compare');
 
 const testRepository = new Repository(getTestConnectionPool());
 const noop = () => {};
@@ -16,21 +16,21 @@ beforeAll(async () => {
   await testRepository.executeQuery(
     `insert into teachers (email)
     values
-      ("mrs.streibel@teachers.com"),
       ("mr.garrison@teachers.com"),
-      ("mr.mackey@teachers.com");`,
+      ("mr.mackey@teachers.com"),
+      ("mrs.streibel@teachers.com")`,
     noop
   );
 
   await testRepository.executeQuery(
     `insert into students (email)
     values
-      ("kenny@students.com"),
+      ("bebe@students.com"),
       ("eric@students.com"),
+      ("kenny@students.com"),
       ("kyle@students.com"),
       ("stan@students.com"),
-      ("wendy@students.com"),
-      ("bebe@students.com");`,
+      ("wendy@students.com")`,
     noop
   );
 });
@@ -42,10 +42,10 @@ afterAll(() => {
 });
 
 describe('Repository - Registrations table', () => {
-  test('registerStudents() should insert records into the database', async () => {
+  test('registerStudents(...) should register kyle to mr garrison', async () => {
     await testRepository.registerStudents(noop, {
       teacher: 'mr.garrison@teachers.com',
-      students: ['kyle@students.com', 'stan@students.com'],
+      students: ['kyle@students.com'],
     });
 
     const results = await testRepository.executeQuery(
@@ -60,7 +60,39 @@ describe('Repository - Registrations table', () => {
       .map((r) =>
         filterObject(r, ['teacher_email', 'student_email', 'student_suspended'])
       )
-      .sort(sortResultsByKey('teacher_email'));
+      .sort(sortResultsBy2Keys(['teacher_email', 'student_email']));
+
+    const expected = [
+      {
+        teacher_email: 'mr.garrison@teachers.com',
+        student_email: 'kyle@students.com',
+        student_suspended: 0,
+      },
+    ];
+
+    expect(results.length).toEqual(1);
+    expect(actualSorted).toStrictEqual(expected);
+  });
+
+  test('registerStudents(...) should register kyle and eric to mrs streibel', async () => {
+    await testRepository.registerStudents(noop, {
+      teacher: 'mrs.streibel@teachers.com',
+      students: ['kyle@students.com', 'eric@students.com'],
+    });
+
+    const results = await testRepository.executeQuery(
+      `select teachers.email as teacher_email, students.email as student_email, students.suspended as student_suspended
+        from registrations
+        inner join teachers on registrations.teacher_id = teachers.id
+        inner join students on registrations.student_id = students.id`,
+      noop
+    );
+
+    const actualSorted = results
+      .map((r) =>
+        filterObject(r, ['teacher_email', 'student_email', 'student_suspended'])
+      )
+      .sort(sortResultsBy2Keys(['teacher_email', 'student_email']));
 
     const expected = [
       {
@@ -69,13 +101,79 @@ describe('Repository - Registrations table', () => {
         student_suspended: 0,
       },
       {
-        teacher_email: 'mr.garrison@teachers.com',
-        student_email: 'stan@students.com',
+        teacher_email: 'mrs.streibel@teachers.com',
+        student_email: 'eric@students.com',
+        student_suspended: 0,
+      },
+      {
+        teacher_email: 'mrs.streibel@teachers.com',
+        student_email: 'kyle@students.com',
         student_suspended: 0,
       },
     ];
 
-    expect(results.length).toEqual(2);
+    expect(results.length).toEqual(3);
+    expect(actualSorted).toStrictEqual(expected);
+  });
+
+  test('registerStudents(...) should register wendy, kyle and eric to mr mackey', async () => {
+    await testRepository.registerStudents(noop, {
+      teacher: 'mr.mackey@teachers.com',
+      students: [
+        'kyle@students.com',
+        'eric@students.com',
+        'wendy@students.com',
+      ],
+    });
+
+    const results = await testRepository.executeQuery(
+      `select teachers.email as teacher_email, students.email as student_email, students.suspended as student_suspended
+        from registrations
+        inner join teachers on registrations.teacher_id = teachers.id
+        inner join students on registrations.student_id = students.id`,
+      noop
+    );
+
+    const actualSorted = results
+      .map((r) =>
+        filterObject(r, ['teacher_email', 'student_email', 'student_suspended'])
+      )
+      .sort(sortResultsBy2Keys(['teacher_email', 'student_email']));
+
+    const expected = [
+      {
+        teacher_email: 'mr.garrison@teachers.com',
+        student_email: 'kyle@students.com',
+        student_suspended: 0,
+      },
+      {
+        teacher_email: 'mr.mackey@teachers.com',
+        student_email: 'eric@students.com',
+        student_suspended: 0,
+      },
+      {
+        teacher_email: 'mr.mackey@teachers.com',
+        student_email: 'kyle@students.com',
+        student_suspended: 0,
+      },
+      {
+        teacher_email: 'mr.mackey@teachers.com',
+        student_email: 'wendy@students.com',
+        student_suspended: 0,
+      },
+      {
+        teacher_email: 'mrs.streibel@teachers.com',
+        student_email: 'eric@students.com',
+        student_suspended: 0,
+      },
+      {
+        teacher_email: 'mrs.streibel@teachers.com',
+        student_email: 'kyle@students.com',
+        student_suspended: 0,
+      },
+    ];
+
+    expect(results.length).toEqual(6);
     expect(actualSorted).toStrictEqual(expected);
   });
 });
--- server/utils/compare.js
+++ server/utils/compare.js
@@ -10,6 +10,26 @@ const sortResultsByKey = (key) => (a, b) => {
   return 0;
 };
 
+const sortResultsBy2Keys =
+  ([key1, key2]) =>
+  (a, b) => {
+    if (a[key1] < b[key1]) {
+      return -1;
+    }
+    if (a[key1] > b[key1]) {
+      return 1;
+    }
+
+    // a[key1] === b[key1], sort by second key
+    if (a[key2] < b[key2]) {
+      return -1;
+    }
+    if (a[key2] > b[key2]) {
+      return 1;
+    }
+    return 0;
+  };
+
 const filterObject = (obj, keys) =>
   keys.reduce((acc, k) => {
     acc[[k]] = obj[k];
@@ -18,5 +38,6 @@ const filterObject = (obj, keys) =>
 
 module.exports = {
   sortResultsByKey,
+  sortResultsBy2Keys,
   filterObject,
 };
